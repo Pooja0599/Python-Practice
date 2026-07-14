29ae17b59ace58921f15195612a60dd18f340512
Pooja Singh <poojasisodiya19981999@gmail.com>
2026-07-14 22:32:33 +0530

    Add files via upload

---
+++ 01_Python_Basics.py
@@ -0,0 +1,105 @@
+# ==========================================
+# Topic : Python Basics
+# GitHub : Pooja0599
+# ==========================================
+
+# 1. Print Statement :- 
+print("Hello, World!")
+print("Welcome to Python")
+print("===============================")
+
+
+# 2. Variables :- 
+name = "Pooja"      
+age = 27             
+salary = 35000.50   
+is_student = False   
+
+print(name)
+print(age)
+print(salary)
+print(is_student)
+print("===============================")
+
+
+# 3. Data Types :- 
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_student))
+print("===============================")
+
+
+# 4. Taking User Input :- 
+city = input("Enter your city : ")
+print("City :", city)
+print("===============================")
+
+
+# 5. Type Casting :- 
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("Sum =", num1 + num2)
+print("===============================")
+
+
+# 6. Arithmetic Operators :- 
+a = 20
+b = 10
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)
+print("===============================")
+
+
+# 7. Comparison Operators :- 
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print("===============================")
+
+
+# 8. Logical Operators :- 
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
+print("===============================")
+
+
+# 9. Assignment Operators :- 
+c = 10
+c += 5
+print(c)
+print("===============================")
+
+
+# 10. Identity Operators :- 
+list1 = [1, 2, 3]
+list2 = list1
+list3 = [1, 2, 3]
+
+print(list1 is list2)
+print(list1 is list3)
+print("===============================")
+
+
+# 11. Membership Operators :- 
+fruits = ["Apple", "Banana", "Mango"]
+
+print("Apple" in fruits)
+print("Orange" not in fruits)
+print("===============================")
+
+# ====================================================================================
+# End of Python Basics
